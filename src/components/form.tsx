4aa76e8733900ca72c8b91038a008398c904bf8d
Ali <mfateminia@gmail.com>
2020-05-10 10:30:13 +0200

    chore: wip

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -11,6 +11,8 @@ interface RequestFormState {
   email: string;
   dialCode: string;
   phone: string;
+  longitude: string;
+  latitude: string;
 }
 
 interface RequestFormProps {
@@ -30,65 +32,94 @@ export default class RequestForm extends Component<
       email: '',
       dialCode: '',
       phone: '',
+      longitude: '',
+      latitude: '',
     };
   }
+
+  public onSubmit = async (event) => {
+    //   setLoading(true);
+    event.preventDefault();
+
+    try {
+      const profile: ProfileRequestCreation = {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        email: this.state.email,
+        address: {
+          location: {
+            type: 'Point',
+            coordinates: [
+              parseFloat(this.state.longitude as any),
+              parseFloat(this.state.latitude as any),
+            ],
+          },
+          raw: this.state.address,
+        },
+        phone: { number: this.state.dialCode, dialCode: this.state.phone },
+      };
+
+      const response = await createRequest(profile);
+
+      console.log({ response });
+    } catch (e) {
+      throw e;
+    }
+  };
+
+  public onAddressChange = () => {
+    console.log(1);
+  };
 
   public render(
     { color },
     { firstName, lastName, address, email, dialCode, phone }
   ) {
-    const onSubmit = async (event) => {
-      //   setLoading(true);
-      event.preventDefault();
-
-      try {
-        const profile: ProfileRequestCreation = {
-          firstName: this.state.firstName,
-          lastName: this.state.lastName,
-          email: this.state.email,
-          address: {
-            location: {
-              type: 'Point',
-              coordinates: [
-                0,
-                0,
-                // parseFloat(this.state.longitude as any),
-                // parseFloat(this.state.latitude as any),
-              ],
-            },
-            raw: this.state.address,
-          },
-          phone: { number: this.state.dialCode, dialCode: this.state.phone },
-        };
-
-        const response = await createRequest(profile);
-
-        console.log({ response });
-      } catch (e) {
-        throw e;
-      }
-    };
-
     return (
-      <form onSubmit={onSubmit}>
+      <form onSubmit={this.onSubmit}>
         <div class="row">
           <div class="field">
             <label>First name</label>
-            <input type="text" value={this.state.firstName}></input>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e: any) => {
+                this.setState({ firstName: e.target.value });
+              }}
+            />
           </div>
           <div class="field">
             <label>Last name</label>
-            <input type="text" value={this.state.lastName}></input>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e: any) => {
+                this.setState({ lastName: e.target.value });
+              }}
+            />
           </div>
         </div>
         <div class="row">
           <div class="field">
             <label>Address (Street &#38; Number)</label>
-            <input type="text" value={this.state.address}></input>
+            <input
+              type="text"
+              value={address}
+              onChange={(e: any) => {
+                this.setState({ address: e.target.value });
+                this.onAddressChange();
+              }}
+            />
           </div>
           <div class="field">
             <label>Email</label>
-            <input type="text" value={this.state.email}></input>
+            <input
+              type="text"
+              value={email}
+              onChange={(e: any) => {
+                this.setState({ email: e.target.value });
+              }}
+            />
           </div>
         </div>
         <div class="row">
@@ -99,7 +130,7 @@ export default class RequestForm extends Component<
                 <option>{`${c.name} (${c.dialCode})`}</option>
               ))}
             </select>
-            <input type="text" value={this.state.phone}></input>
+            <input type="text" value={phone} />
           </div>
         </div>
         <button type="submit">Send a request</button>
